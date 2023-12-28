817b04a37f40af1f7b93372dce051ab0d3c0bcc3
nilsniklavsplumins <147701015+nilsniklavsplumins@users.noreply.github.com>
2023-12-28 14:53:04 +0200

    Pievienot lauku iztīrīšanu claimCoinGecko()

--- main.py
+++ main.py
@@ -39,7 +39,9 @@ def claimCoinGecko(sb):
         
     sb.open(url)
     sb.maximize_window()
+    sb.clear("#user_email")
     sb.type("#user_email", next(account["username"] for account in secret if account["id"] == "coingecko"))
+    sb.clear("#user_password")
     sb.type("#user_password", next(account["password"] for account in secret if account["id"] == "coingecko"))
     sb.click("button[type='submit']")
     sb.sleep(1)

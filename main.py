4e9c27c2c701172538fb6529b7bec2a0912b11ae
nilsniklavsplumins <147701015+nilsniklavsplumins@users.noreply.github.com>
2023-12-28 14:45:05 +0200

    Izveidot balvu savākšanas funkciju CoinGecko

--- main.py
+++ main.py
@@ -34,5 +34,17 @@ def claimAllkeyshop(sb):
             sb.sleep(0.5)
         sb.sleep(5)
 
+def claimCoinGecko(sb):
+    url = "https://www.coingecko.com/account/candy"
+        
+    sb.open(url)
+    sb.maximize_window()
+    sb.type("#user_email", next(account["username"] for account in secret if account["id"] == "coingecko"))
+    sb.type("#user_password", next(account["password"] for account in secret if account["id"] == "coingecko"))
+    sb.click("button[type='submit']")
+    sb.sleep(1)
+    sb.click("button[type='submit']")
+    
 with SB(uc_cdp=True, guest_mode=True) as sb:
     claimAllkeyshop(sb)
+    claimCoinGecko(sb)

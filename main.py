5b660d7b849306b84b2fe748374a79af5f963a9f
nilsniklavsplumins <147701015+nilsniklavsplumins@users.noreply.github.com>
2023-12-29 17:30:01 +0200

    Sadalīt claimAllkeyshop funkciju loģiskos posmos

--- main.py
+++ main.py
@@ -39,12 +39,14 @@ def claimCoinGecko(sb):
         
     sb.open(url)
     sb.maximize_window()
+
     sb.clear("#user_email")
     sb.type("#user_email", next(account["username"] for account in secret if account["id"] == "coingecko"))
     sb.clear("#user_password")
     sb.type("#user_password", next(account["password"] for account in secret if account["id"] == "coingecko"))
     sb.click("button[type='submit']")
     sb.sleep(1)
+
     sb.click("button[type='submit']")
     
 with SB(uc_cdp=True, guest_mode=True) as sb:
